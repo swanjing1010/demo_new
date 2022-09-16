8bae102e863e19b83057d2841774086bd8a2ee37
Wanjing Shi <swanjing@umich.edu>
2022-09-16 17:07:42 -0400

    Brought the initial version of the script back

--- problem1.sh
+++ problem1.sh
@@ -1,6 +1,4 @@
-echo BEGIN PROCESSING!
 head -n 4 multi_seq.fa > seq1.fa
 head -n 8 multi_seq.fa | tail -n 4 > seq2.fa
 head -n 12 multi_seq.fa | tail -n 4 > seq3.fa
 head -n 16 multi_seq.fa | tail -n 4 > seq4.fa
-echo DONE!
