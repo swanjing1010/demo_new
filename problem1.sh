4ef9971aff19e250c7b910944129390e04df2a43
swanjing1010 <112363427+swanjing1010@users.noreply.github.com>
2022-09-16 16:34:45 -0400

    Added two echo lines start and done in the script

--- problem1.sh
+++ problem1.sh
@@ -1,4 +1,6 @@
+echo STARTING!
 head -n 4 multi_seq.fa > seq1.fa
 head -n 8 multi_seq.fa | tail -n 4 > seq2.fa
 head -n 12 multi_seq.fa | tail -n 4 > seq3.fa
 head -n 16 multi_seq.fa | tail -n 4 > seq4.fa
+echo DONE!
